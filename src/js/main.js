0ac4c3febdef033f1ae9fa641798520fe3c134e5
Ranya <ranyapolisoidis2001@gmail.com>
2023-12-20 11:57:24 +0100

    création de ma section 6+ Navs and tabs en js

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,8 @@ import {lidDropdown} from "./modules/dropdown.js";
 import {cube1,cube2,cube3,cube4,cube5,cube6} from "./modules/animation.js";
 
 import {btnCarousel1,btnCarousel2,btnCarousel3,btnCarousel4,btnCarousel5} from "./modules/carousel.js";
+
+import {btn1NavsAndTabs,btn2NavsAndTabs,btn3NavsAndTabs} from "./modules/navsAndTabs.js";
+
+
+
---
+++ src/js/modules/navsAndTabs.js
@@ -0,0 +1,47 @@
+// ###########################################################################################################################################################################################
+// Navs and tabs
+// ###########################################################################################################################################################################################
+
+
+let btnAllSection6 = document.querySelectorAll(".btnAllSection6")
+
+let btn1NavsAndTabs = btnAllSection6[0]
+let btn2NavsAndTabs = btnAllSection6[1]
+let btn3NavsAndTabs = btnAllSection6[2]
+
+let divIA = document.querySelector(".divIA")
+let divDev = document.querySelector(".divDev")
+let divUIUX = document.querySelector(".divUIUX")
+
+btn1NavsAndTabs.addEventListener("click", function () {
+    divIA.style.display = "block "
+    divDev.style.display = "none"
+    divUIUX.style.display = "none"
+
+
+    btn1NavsAndTabs.style.textDecorationColor="rgb(55, 56, 58)"
+    btn2NavsAndTabs.style.textDecorationColor="rgb(142, 144, 150)"
+    btn3NavsAndTabs.style.textDecorationColor="rgb(142, 144, 150)"
+})
+
+btn2NavsAndTabs.addEventListener("click", function () {
+    divIA.style.display = "none"
+    divDev.style.display = "block"
+    divUIUX.style.display = "none"
+
+    btn1NavsAndTabs.style.textDecorationColor="rgb(142, 144, 150)"
+    btn2NavsAndTabs.style.textDecorationColor="rgb(55, 56, 58)"
+    btn3NavsAndTabs.style.textDecorationColor="rgb(142, 144, 150)"
+})
+
+btn3NavsAndTabs.addEventListener("click", function () {
+    divIA.style.display = "none"
+    divDev.style.display = "none"
+    divUIUX.style.display = "block"
+
+    btn1NavsAndTabs.style.textDecorationColor="rgb(142, 144, 150)"
+    btn2NavsAndTabs.style.textDecorationColor="rgb(142, 144, 150)"
+    btn3NavsAndTabs.style.textDecorationColor="rgb(55, 56, 58)"
+})
+
+export { btn1NavsAndTabs, btn2NavsAndTabs, btn3NavsAndTabs }
